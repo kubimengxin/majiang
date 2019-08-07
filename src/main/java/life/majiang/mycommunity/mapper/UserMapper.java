678a07f4999c374b13486ac8e3d35e3eaf798301
kubimengxin <2650122405@qq.com>
2019-08-07 09:58:23 +0800

    add check cookies

--- src/main/java/life/majiang/mycommunity/mapper/UserMapper.java
+++ src/main/java/life/majiang/mycommunity/mapper/UserMapper.java
@@ -3,10 +3,16 @@ package life.majiang.mycommunity.mapper;
 import life.majiang.mycommunity.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper
 public interface UserMapper{
 
     @Insert("INSERT INTO user(name,account_id,token,gmt_create,gmt_modified)values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * from user where token = #{token}")
+    User findByToken(String token);
 }
