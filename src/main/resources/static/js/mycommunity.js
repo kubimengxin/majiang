c9cb8537f9a26580d177cb29714356bcef387c2b
kubimengxin <2650122405@qq.com>
2020-04-12 16:26:58 +0800

    实现问题回复-页面提交回复

---
+++ src/main/resources/static/js/mycommunity.js
@@ -0,0 +1,29 @@
+/**
+ * @author 苦逼萌新
+ * @date 4/12/2020
+ */
+
+function post() {
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type:"POST",
+        url: "/comment",
+        contentType: "application/json",
+        data:JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if(response.code == 200){
+                $("#comment_section").hide();
+            }else{
+                alert(response.message());
+            }
+            console.log(response)
+        },
+        dataType: "json"
+    });
+
+}
